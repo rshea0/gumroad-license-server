2a34fd7c9b6470025d319e8f4084c9430c84bd7e
Ryan Shea <shayded@shayded.com>
2021-05-09 16:25:32 -0500

    ci: add await in build script

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const env = process.env;
 
-fs.promises.writeFile(
+await fs.promises.writeFile(
   path.resolve(__dirname, '../env.json'),
   JSON.stringify({
     GUMROAD_API: env.GUMROAD_API,
