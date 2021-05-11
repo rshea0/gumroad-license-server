864de341aefb63708b24f143674f7b59c765b76e
Ryan Shea <shayded@shayded.com>
2021-05-10 20:06:36 -0500

    ci: fix build script

--- scripts/build.js
+++ scripts/build.js
@@ -3,11 +3,15 @@ const path = require('path');
 
 const env = process.env;
 
-await fs.promises.writeFile(
-  path.resolve(__dirname, '../env.json'),
-  JSON.stringify({
-    GUMROAD_API: env.GUMROAD_API,
-    GUMROAD_PRODUCT_ID: env.GUMROAD_PRODUCT_ID,
-    LICENSE_PRIVATE_KEY: env.LICENSE_PRIVATE_KEY,
-  }),
-);
+async function run() {
+  await fs.promises.writeFile(
+    path.resolve(__dirname, '../env.json'),
+    JSON.stringify({
+      GUMROAD_API: env.GUMROAD_API,
+      GUMROAD_PRODUCT_ID: env.GUMROAD_PRODUCT_ID,
+      LICENSE_PRIVATE_KEY: env.LICENSE_PRIVATE_KEY,
+    }),
+  );
+}
+
+run().catch(e => console.error(e));
