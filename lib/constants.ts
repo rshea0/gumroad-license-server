8d06e79e30c5938cfe2b49e103b2753b11685101
Ryan Shea <shayded@shayded.com>
2021-06-14 03:59:12 -0500

    feat: put back old product permalink

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,5 +3,6 @@ export const GUMROAD_API = 'https://api.gumroad.com/v2';
 export const PRODUCT_PERMALINKS: {
   [id: string]: string | undefined;
 } = {
-  vxJlA: 'tuneup-prime',
+//   vxJlA: 'tuneup-prime',
+  vxJlA: 'enjinn',
 };
