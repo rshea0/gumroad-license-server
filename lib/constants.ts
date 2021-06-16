07f124c6848e963b09ca3b0915ef9482bc7b6fd7
Ryan Shea <shayded@shayded.com>
2021-06-16 18:20:07 -0500

    feat: update gumroad permalink

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,6 +3,5 @@ export const GUMROAD_API = 'https://api.gumroad.com/v2';
 export const PRODUCT_PERMALINKS: {
   [id: string]: string | undefined;
 } = {
-//   vxJlA: 'tuneup-prime',
-  vxJlA: 'enjinn',
+  vxJlA: 'tuneup-prime',
 };
